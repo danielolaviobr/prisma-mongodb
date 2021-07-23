499e6b0a5cd7a48d9bfc4bd865bebafb1f9e1fee
Daniel <danielolaviobr@gmail.com>
2021-07-23 22:11:12 +0100

    Sample server

--- src/server.ts
+++ src/server.ts
@@ -8,15 +8,16 @@ const app = express();
 app.use(json());
 
 app.get("/", async (req, res) => {
-	const data = await prisma.user.create({
-		data: { name: "Daniel Alves", email: "daniel@email.com" },
-	});
-
-	return res.json({ data });
+	return res.send("Well done!");
 });
 
-app.post("/", (req, res) => {
-	res.send("Well done!");
+app.post("/", async (req, res) => {
+	const { email, name, street, country } = req.body;
+	const user = await prisma.user.create({
+		data: { email, name, address: { create: { street, country } } },
+		include: { address: true },
+	});
+	return res.json({ user });
 });
 
 app.listen(3000, () => {
